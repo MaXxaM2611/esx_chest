84e7176fac366405f7ef0a7c35f5b3ff0601d236
MaXxaM <64220045+MaXxaM2611@users.noreply.github.com>
2020-07-19 22:01:03 +0200

    Delete it.lua

--- it.lua
+++
@@ -1,9 +0,0 @@
---###################################
---## ESX_CHEST By MaXxaM#0511 ####
---###################################
-
-Locales['it'] = {
-
-	['press_to_action'] = 'Premi ~INPUT_CONTEXT~ per interagire con il ~b~magazzino',
-	
-}
